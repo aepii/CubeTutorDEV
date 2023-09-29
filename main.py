7860b88ac3318938b8f326a49e8f70d14a71f6ca
aepii <68669356+aepii@users.noreply.github.com>
2023-09-28 22:44:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,22 @@
 from cube.cube import CubeBuilder
 from cube.cube_moves import CubeMoves
-
 from cube.cube_render import CubeRender
-
-import numpy as np
-import cv2
 
 
 def main():
-    cube1 = CubeBuilder() \
-        .updateFront([[2, 1, 1], [0, 0, 1], [5, 2, 0]]) \
-        .updateBack() \
-        .updateLeft() \
-        .updateRight() \
-        .updateUpper() \
-        .updateLower()
+    myCube = CubeBuilder(default=True)
 
-    cube1.updateFront()
-
-    cube2 = CubeBuilder(default=True)
-
-    print(cube1.Cube)
-    print(cube2.Cube)
-
-    print(cube2.Cube.color_visualization())
+    print(myCube.Cube.color_visualization())
 
     cubeMoves = CubeMoves().add_moves("L' B2 D' F R2 L' F2 D R D2 F2'")
 
-    cubeMoves.execute_moves(cube2.Cube)
+    cubeMoves.execute_moves(myCube.Cube)
 
-    print(cube2.Cube)
+    print(myCube.Cube.color_visualization())
 
-    print(cube2.Cube.color_visualization())
+    print(myCube.Cube.history, cubeMoves.moves)
 
-    print(cubeMoves.history, cubeMoves.moves)
-
-    newRender = CubeRender(cube2.Cube)
+    newRender = CubeRender(myCube.Cube)
     newRender.render()
 
 
